6d9c66b29893016fa2b2d741d21c01dcf2903b5b
Greg Gibeling <gdgib@outlook.com>
2024-08-09 07:45:35 -0700

    G2-1615 Test & debug

--- mkdocker/scripts/mkdocker.sh
+++ mkdocker/scripts/mkdocker.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -eu -o pipefaile
+set -eu -o pipefail
 
 cd /mkdocker/docs
 if [ -d .git ]; then
 	git pull
 else
 	git clone "https://${MKDOCKER_REPOSITORY_URL}" .
+	git checkout "${MKDOCKER_REPOSITORY_BRANCH}"
 fi
 
 cd "${MKDOCKER_REPOSITORY_DIRECTORY}"
